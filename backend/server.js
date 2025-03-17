e61e92b32ded5b89d7cf5f61c33765b3fdcc53c1
alaguthiyagarajan <alaguthiyagarajana@gmail.com>
2025-03-17 16:44:46 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "https://mern-stack-frontend-beryl.vercel.app",
+    origin: "http://localhost:3000",
     credentials: true,
     methods: "GET, POST, PUT, DELETE",
     allowedHeaders: ["Content-Type", "Authorization"]
