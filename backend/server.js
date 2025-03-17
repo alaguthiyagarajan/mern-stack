6bcdc593d1016cb0180d9d33bdb410b74afe737f
Alaguthiyagarajan <132125380+alaguthiyagarajan@users.noreply.github.com>
2025-03-17 14:43:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,12 @@ const User = require('./models/User');
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://mern-stack-frontend-beryl.vercel.app");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
-});
+app.use(cors({
+    origin: "https://mern-stack-frontend-beryl.vercel.app",
+    credentials: true,
+    methods: "GET, POST, PUT, DELETE",
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 app.use('/uploads', express.static('uploads'));
 
