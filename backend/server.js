224281a6a35c0daa26bb1ddcaa545269b6bfdb79
Alaguthiyagarajan <132125380+alaguthiyagarajan@users.noreply.github.com>
2025-03-17 14:37:20 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,13 @@ const User = require('./models/User');
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ credentials: true, origin: "https://mern-stack-frontend-beryl.vercel.app" }));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://mern-stack-frontend-beryl.vercel.app");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+
 app.use('/uploads', express.static('uploads'));
 
 // MongoDB Connection
